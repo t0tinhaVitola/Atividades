bf13e8b6ec328589413745cecf1746a6df4f1a70
t0tinhaVitola <otaviobvoficial2005@gmail.com>
2025-10-29 18:40:25 -0300

    Update BinarySearch.c

--- AED1/LeetCode/BinarySearch.c
+++ AED1/LeetCode/BinarySearch.c
@@ -7,8 +7,8 @@ int searchInsert(int* nums, int numsSize, int target) {
     int left = 0, right = numsSize, mid;
 
     while    ( left < right )    {
-        mid = (left + right) / 2;
-        if    ( target <= nums[mid] )    {
+        mid = ( left + right ) / 2;
+        if    ( target <= nums[ mid ] )    {
             right = mid;
         }    else    {
             left = mid + 1;
