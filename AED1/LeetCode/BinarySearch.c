3693d4523930b1e6d880ff229cc441395f2faf33
t0tinhaVitola <otaviobvoficial2005@gmail.com>
2025-10-29 18:39:49 -0300

    Create BinarySearch.c

---
+++ AED1/LeetCode/BinarySearch.c
@@ -0,0 +1,20 @@
+/*
+A solution to the LeetCode Problem "Search Insert Position", which, performs a search on a sorted array for a given target.
+
+*/
+
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0, right = numsSize, mid;
+
+    while    ( left < right )    {
+        mid = (left + right) / 2;
+        if    ( target <= nums[mid] )    {
+            right = mid;
+        }    else    {
+            left = mid + 1;
+        }
+
+    }
+
+    return left;
+}
