bbf57cfed457a98f2848815d3df40fdc658c4cc9
t0tinhaVitola <otaviobvoficial2005@gmail.com>
2026-01-25 23:55:45 -0300

    Create SameTree.c

---
+++ AED1/LeetCode/SameTree.c
@@ -0,0 +1,26 @@
+/*
+A solution to the LeetCode problem "Same Tree", which, given the roots of two binary trees, returns if they are same or not.
+
+*/
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if ( p == NULL && q == NULL )
+        return true;
+
+    if ( p == NULL || q == NULL )
+        return false;
+    
+    if ( p->val != q->val )
+        return false;
+    
+    
+    if ( !isSameTree( p->left, q->left ) )
+        return false;
+
+    if ( !isSameTree( p->right, q->right ) )
+        return false;
+
+    return true;
+
+}
