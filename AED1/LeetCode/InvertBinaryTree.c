6ea1353c2dc01b993ab250097d7506e8a8f68269
t0tinhaVitola <otaviobvoficial2005@gmail.com>
2026-01-25 23:33:36 -0300

    Update InvertBinaryTree.c

--- AED1/LeetCode/InvertBinaryTree.c
+++ AED1/LeetCode/InvertBinaryTree.c
@@ -1,1 +1,17 @@
+/*
+A solution to the LeetCode problem, which, given the root of a binary tree, invert the tree, and return its root.
 
+*/
+
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if ( root == NULL ) 
+        return NULL;
+    
+    struct TreeNode* leftRoot = invertTree(root->left);
+    struct TreeNode* rightRoot = invertTree(root->right);
+
+    root->right = leftRoot;
+    root->left = rightRoot;
+
+    return root;
+}
